d2e0af50cc054a9966d1b9f24b495ecf628bc3d4
sara nicolay <nicolay.sara@gmail.com>
2020-02-24 17:06:33 +0100

    remove useless actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :destroy]
 
   def show
-    @user = User.find(params[:id])
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-    #   redirect_to user_path(user)
-    # else
-    #   render :create
-    end
-  end
-
-  def destroy
-    @user.destroy
-    # redirect_to
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
-
-  def set_user
     @user = User.find(params[:id])
   end
 
