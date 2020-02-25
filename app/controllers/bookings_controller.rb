35ff09968669efe51263958ee2706e2ffce8f2f6
Coline Bricteux <coline.bricteux@gmail.com>
2020-02-25 11:20:20 +0100

    bookings controller et sources ok

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,37 @@
+class BookingsController < ApplicationController
+
+  # def index
+  #   @bookings = Booking.all
+  # end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @user = User.find(params[:user_id])
+    @toilet = Toilet.find(params[:toilet_id])
+    @booking.user = @user
+    @booking.toilet = @toilet
+    @booking.save
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:date)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,11 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resources :toilets, only: [:new, :create, :edit, :update, :destroy]
+    resources :bookings, only: [:show, :destroy]
   end
 
-    resources :toilets, only: [:index, :show]
+  resources :toilets, only: [:index, :show] do
+    resources :bookins, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
