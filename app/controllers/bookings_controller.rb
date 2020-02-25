48b0b16cc94dfee0936054951d8a12bb70c283fc
Yann Lauwers <yann.lauwers@icloud.com>
2020-02-25 14:55:41 +0100

    corrected navbar and added route index of toilets

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,10 @@ class BookingsController < ApplicationController
   #   @bookings = Booking.all
   # end
 
+  def index
+    @bookings = Booking.all
+  end
+
   def show
     @booking = Booking.find(params[:id])
   end
