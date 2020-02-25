7ce92edc586ffad07b9e4aa4b0106c8324d9c1cd
Yann Lauwers <yann.lauwers@icloud.com>
2020-02-25 15:51:35 +0100

    added a column and started the process to add images to cloudinary

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -1,7 +1,9 @@
 class Toilet < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :image
 
   validates :name, presence: true
   validates :address, presence: true
+  validates :image, presence: true
 end
