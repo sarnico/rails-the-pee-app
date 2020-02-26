3553857e157ee0184a9f8d7b1805c6c86ff7f19b
Coline Bricteux <coline.bricteux@gmail.com>
2020-02-26 15:18:48 +0100

    petits updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_144849) do
+ActiveRecord::Schema.define(version: 2020_02_26_103219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 2020_02_25_144849) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "image"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_toilets_on_user_id"
   end
 
