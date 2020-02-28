3e3814dd9efdc1e29dcc44a489a6fdd6603d8352
Yann Lauwers <yann.lauwers@icloud.com>
2020-02-28 16:05:23 +0100

    bvfer

--- app/javascript/plugins/input_search.js
+++ app/javascript/plugins/input_search.js
@@ -3,22 +3,24 @@ const inputSearch = () => {
 
   let mapSearch = document.querySelector('.mapboxgl-ctrl-geocoder--input');
 
-  mapSearch.addEventListener('keyup', (event) => {
-    if (event.keyCode == 13) {
-      const test = event.currentTarget.value
-      console.log(test)
+  if ((mapSearch)) {
+    mapSearch.addEventListener('keyup', (event) => {
+      if (event.keyCode == 13) {
+        const test = event.currentTarget.value
+        console.log(test)
 
-      fetch('near_toilets?location_query=' + test)
-        .then(response => response.json())
-        .then(json => {
-          const allToilets = document.querySelector(".all-toilets")
-          allToilets.innerHTML = ""
-          json.forEach(entry => {
-            allToilets.innerHTML += entry.partial
+        fetch('near_toilets?location_query=' + test)
+          .then(response => response.json())
+          .then(json => {
+            const allToilets = document.querySelector(".all-toilets")
+            allToilets.innerHTML = ""
+            json.forEach(entry => {
+              allToilets.innerHTML += entry.partial
+            })
           })
-        })
-    };
-  });
+      };
+    });
+  }
 
 
 
