f711857ca8a682780db7c8ea3c5e6c6b0c9363e8
Yann Lauwers <yann.lauwers@icloud.com>
2020-02-28 16:15:41 +0100

    Fix null el

--- app/javascript/plugins/submit_button_booking.js
+++ app/javascript/plugins/submit_button_booking.js
@@ -1,9 +1,12 @@
 function show_prompt() {
-  document.querySelector(".submit-booking").addEventListener("click", (event) => {
-    // event.preventDefault();
-    const name = alert('Your booking has been taken in account');
-    window.location.href = "https://thepeeapp.herokuapp.com/"
-  });
+  const ois = document.querySelector(".submit-booking")
+  if(ois){
+    ois.addEventListener("click", (event) => {
+      // event.preventDefault();
+      const name = alert('Your booking has been taken in account');
+      window.location.href = "https://thepeeapp.herokuapp.com/"
+    });
+  }
 }
 
 
